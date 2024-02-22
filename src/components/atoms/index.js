8dbc86828f21a5b58e7e4954bbadb530c32a93b6
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-22 21:12:07 +0700

    Create index.js

---
+++ src/components/atoms/index.js
@@ -0,0 +1,7 @@
+import Blur from './Blur/Blur.css';
+import BottomLine from './BottomLine';
+import PrimaryBtn from './PrimaryBtn';
+import ScrollToTop from './ScrollToTop';
+import SecondaryBtn from './SecondaryBtn';
+
+export { Blur, BottomLine, PrimaryBtn, ScrollToTop, SecondaryBtn };
