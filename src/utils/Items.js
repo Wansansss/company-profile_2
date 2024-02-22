4edf7e23265d5f941659018fe94a6e7644ede159
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-22 21:13:11 +0700

    Create Items.js

---
+++ src/utils/Items.js
@@ -0,0 +1,72 @@
+import layananIcon from "../assets/images/layanan.png"
+
+const Items = [
+  {
+    id: 1,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Menyediakan berbagai macam produk perangkat elektronik yang digunakan oleh pelanggan individu dan keluarga untuk kebutuhan sehari-hari dirumah dengan berbagai merek dan model",
+  },
+  {
+    id: 2,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Menyajikan lingkungan belanja yang nyaman dan menyenangkan, baik di toko fisik maupun melalui platform online.",
+  },
+  {
+    id: 3,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Menyediakan layanan pelanggan yang responsive untuk membantu pelanggan dalam pertanyaan, masalah, atau bantuan teknis.",
+  },
+  {
+    id: 4,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Memberikan pelayanan dengan respon cepat dan tepat terhadap pelanggan.",
+  },
+  {
+    id: 5,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Memberikan informasi yang lengkap dan akurat mengenai produk, seperti spesifikasi, fitur, dan ulasan pelanggan.",
+  },
+  {
+    id: 6,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Memberikan fasilitas untuk menguji produk secara langsung (jika memungkinkan) atau memberikan informasi mengenai kebijakan pengembalian barang.",
+  },
+  {
+    id: 7,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Menyediakan layanan pengiriman yang cepat dan aman dengan menggunakan kurir pribadi yang berpengalaman.",
+  },
+  {
+    id: 8,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Memberikan kebijakan garansi pengembalian produk yang bermasalah selama 7 hari terhitung sejak tanggal pembelian kecuali untuk situasi human eror. ",
+  },
+  {
+    id: 9,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Menawarkan promosi diskon produk untuk memberikan nilai tambah kepada pelanggan.",
+  },
+  {
+    id: 10,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Menyediakan relasi dengan service center masing-masing merek untuk layanan purna jual seperti perbaikan atau dukungan teknis setelah pembelian.",
+  },
+  {
+    id: 11,
+    Image:<img src={layananIcon} alt="Icon Layanan"/>,
+    description:
+      "Memberikan informasi edukatif kepada pelanggan tentang penggunaan produk, tips, dan trik, serta pemecahan masalah umum.",
+  },
+];
+
+export default Items;
