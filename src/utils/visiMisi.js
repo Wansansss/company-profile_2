13ec57eaca1c1015d175c795c0d8e8fec510b8fb
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-22 21:13:14 +0700

    Create visiMisi.js

---
+++ src/utils/visiMisi.js
@@ -0,0 +1,26 @@
+import visiIcon from "../assets/images/VisiMisiImg/visi.png"
+import misiIcon from "../assets/images/VisiMisiImg/misi.png"
+
+const visiMisi = [
+  {
+    id: 1,
+    title: "Visi",
+    icon: <img src={visiIcon} alt="icon visi" />,
+    description:
+      "Mengutamakan kepuasan konsumen didukung dengan kualitas Tenaga Kerja Profesional dengan memberikan layanan purna jual yang baik, intensif khusus bagi para konsumen guna pemenuhan kebutuhan retail dibidang jasa elektronik.",
+  },
+  {
+    id: 2,
+    title: "Misi",
+    icon: <img src={misiIcon} alt="icon Misi" />,
+    description1:
+      "1. Mengutamakan kepuasan konsumen dan berpatisipasi aktif dalam pembangunan nasional.",
+    description2:
+      "2. Melayani segala kebutuhan elektronik pembeli baik mulai dari pemesanan hingga pengiriman barang sampai kepada tempat pembeli dengan aman.",
+    description3:
+      "3.Maju dan sejahtera bersama dengan memiliki jaringan yang luas dan tersebar di seluruh wilayah indonesia dalam bidang retail elektronik.",
+  },
+ 
+];
+
+export default visiMisi;
