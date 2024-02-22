49e6b0a1d18b4ba0ba0f3ebc716cd83f146e0776
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-22 21:13:09 +0700

    Create index.js

---
+++ src/pages/Tentang/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TentangKami, VisiMisi,} from "../../components";
+
+const Tentang = () => {
+  return (
+    <div className="">
+      <TentangKami />
+      <VisiMisi />
+    </div>
+  );
+};
+
+export default Tentang;
