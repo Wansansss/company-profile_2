f13360bd573e70397e11ce06008df6c91a741df2
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-22 21:12:29 +0700

    Create index.js

---
+++ src/components/molecules/index.js
@@ -0,0 +1,11 @@
+import Footer from './Footer';
+import Loader from './Loader';
+import Navbar from './Navbar';
+import NotFound from './NotFound';
+import TentangKami from './TentangKami';
+import VisiMisi from './VisiMisi';
+
+export {
+    Footer,Loader,Navbar,
+    NotFound, TentangKami,VisiMisi
+};
