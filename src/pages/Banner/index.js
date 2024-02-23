f2aafb503655b222559901760e8dc6dcc272c763
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-23 14:55:23 +0700

    Update index.js

--- src/pages/Banner/index.js
+++ src/pages/Banner/index.js
@@ -6,7 +6,9 @@ import bannerImg from "../../assets/images/banner.jpg"
 import "../shared/Shared.css"
 import { SecondaryBtn } from "../../components";
 
+
 const Banner = () => {
+
   return (
     <div className="parent min-h-screen mx-auto py-16 flex flex-col-reverse lg:flex-row items-center justify-between bghome">
       <motion.div
@@ -14,21 +16,20 @@ const Banner = () => {
         animate={{ x: 0 }}
         transition={{ duration: 1 }}
       >
-        <h1 className="mb-0 text-4xl font-bold text-black">Sinergi Mandiri Lestari</h1>
+        <h1 className="mb-0 text-4xl font-bold text-white">Sinergi Mandiri Lestari</h1>
         <h2 className="my-6 text-2xl font-bold text-purple-700">
-          Memberikan Produk Elektronik Yang Berkualitas Untuk Anda!
+          BEST SERVICE FOR CUSTOMERS
         </h2>
-        <p className="max-w-md mb-10 font-medium text-accent">
-          Sinergi Mandiri Lestari adalah perusahaan penyedia berbagai macam
-          Produk Elektronik,Pengiriman Cepat,Fasilitas Untuk Menguji Produk dan Memberikan kebijakan garansi pengembalian produk.
+        <p className="max-w-md mb-10 font-medium text-white">
+          Sinergi Mandiri Lestari Berupaya Memberikan Pelayanan Terbaik Untuk Setiap Pelanggan dan Memberikan Produk-Produk Elektronik Dengan Kualitas Terbaik Serta Harga Yang Terjangkau
         </p>
 
         <div className="flex items-center mb-20">
           <Link to="https://ecommercesl.vercel.app" className="ml-4">
             <SecondaryBtn>
-              <span className="text-button-mobile ">Kunjungi Marketplace</span>
+              <span className="text-button-mobile text-white">Kunjungi Marketplace</span>
               <span>
-                <FaAngleRight />
+                <FaAngleRight color="white" />
               </span>
             </SecondaryBtn>
           </Link>
