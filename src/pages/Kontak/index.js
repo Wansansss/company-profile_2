6afc451a85ba888d96adf1661e6185b7b634c1eb
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-28 08:46:37 +0700

    Update index.js

--- src/pages/Kontak/index.js
+++ src/pages/Kontak/index.js
@@ -67,7 +67,7 @@ const Kontak = () => {
           </div>
           <div className="flex items-center my-6">
             <MdEmail className="mr-8 text-2xl duration-300 cursor-pointer hover:text-purple-700"></MdEmail>
-            <h3 className="font-medium text-purple-700">email</h3>
+            <a href="mailto:Sinarlestari555@gmail.com"><h3 className="font-medium text-purple-700">Sinarlestari555@gmail.com</h3></a>
           </div>
         </motion.div>
       </div>
