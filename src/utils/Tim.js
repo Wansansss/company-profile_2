9c3a7fd414cf65d9ab9f68f0785e0cfaa1aacb0e
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-28 08:46:48 +0700

    Create Tim.js

---
+++ src/utils/Tim.js
@@ -0,0 +1,26 @@
+import pict1 from "../assets/images/Tim/Pict_1.png"
+import pict2 from "../assets/images/Tim/Pict_2.png"
+import pict3 from "../assets/images/Tim/Pict_3.png"
+import pict4 from "../assets/images/Tim/Pict_4.png"
+
+
+const Tim = [
+    {
+        id:"1",
+        Image: <img src={pict1} alt=""></img>
+    },
+    {
+        id:"2",
+        Image: <img src={pict2} alt=""></img>
+    },
+    {
+        id:"3",
+        Image: <img src={pict3} alt=""></img>
+    },
+    {
+        id:"4",
+        Image: <img src={pict4} alt=""></img>
+    }
+]
+
+export default Tim
